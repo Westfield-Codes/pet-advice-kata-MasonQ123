bec4838b7640e13bb4764042eea6f95e97a9a410
MasonQ123 <mason117167@student-schoolsofwestfield.org>
2025-09-19 09:46:26 -0400

    Finished kata
    Finished kata works by quizzing for type and then finalizing with a query for the name and outputting the final result

--- petkata.js
+++ petkata.js
@@ -3,13 +3,39 @@
 /* Var, Prompt, Confirm, Alert, Compound Conditional */
 
 // Create pet variable, set equal to empty quotes ("")
-// Ask if they are allergic, store in a boolean variable with OK/Cancel
-// If they are allergic, ask if they want a cute pet, store in a boolean variable with OK/Cancel
-// If they are not allergic, ask if they want to be outdoors with their pet.  Store it in a boolean with OK/Cancel.
-// If allergic and cute, pet =  bird
-// Else if allergic and not cute,  pet =  tarantula
-// Else if not allergic and outdoors,  pet =  dog
-// Else if not allergic and indoors,  pet =  cat 
-// Display the type of pet: You would do well with a pet
-// Ask for a pet name for the pet starting with the same letter as the pet starts with
-// Display petname the pet says hi. 
+var pet = "";
+var allergic = confirm("Are you allergic?");
+//Ask if allergic is true or false
+if (allergic == true) {
+//if allergic is true
+    let cute = confirm("Do you want a cute pet?");
+    //then ask if cute is true
+        if (cute == true){
+            pet = "hamster";
+        }
+        //if cute is true then output hamster
+        else{
+            pet="tarantula";
+        }
+        //otherwise output tarantula
+}
+else {
+//if allergic was false
+    var outdoors = confirm("Do you want to be outdoors with your pet?");
+    //ask if user wants to be outdoors
+        if (outdoors == true){
+            pet = "dog";
+        }
+        //output dog if outdoors is true
+        else{
+            pet = "cat";
+        }
+        //otherwise output cat
+}
+
+alert("You should go with a " + pet + ".")
+//tell user the type of pet
+var pname = prompt("...and what about their name?");
+//ask for a name
+alert(pname + " the " + pet + " says hi!");
+//now finally remind them of the name and type by saying "[Name] the [type of pet] says hi!"
--- script.js
+++ script.js
@@ -8,25 +8,33 @@ var pet = "";
 var allergic = confirm("Are you allergic?");
 // If they are allergic, ask if they want a cute pet, store in a boolean variable with OK/Cancel
 if (allergic == true) {
-    var cute = confirm("Do you want a cute pet?");
-};
-// If they are not allergic, ask if they want to be outdoors with their pet.  Store it in a boolean with OK/Cancel.
-if (allergic == false) {
-    var outdoors = confirm("Do you want to be outdoors with your pet?");
-}; 
-// If allergic and cute, pet =  bird
-if (cute == true) {
-    pet = "bird";
-};
-// Else if allergic and not cute,  pet =  tarantula
-else (pet = "tarantula"){
+    let cute = confirm("Do you want a cute pet?");
+    if (cute == true){
+        pet = "hamster";
+    }
+    else{
+        pet="tarantula";
+    }
 }
+else {
+    var outdoors = confirm("Do you want to be outdoors with your pet?");
+
+if (outdoors == true){
+    pet = "dog";
+}
+else{
+    pet = "cat";
+}
+}
+// If they are not allergic, ask if they want to be outdoors with their pet.  Store it in a boolean with OK/Cancel.
+// If allergic and cute, pet =  bird
+// Else if allergic and not cute,  pet =  tarantula
 // Else if not allergic and outdoors,  pet =  dog
-
 // Else if not allergic and indoors,  pet =  cat 
-
 // Display the type of pet: You would do well with a pet
-
+alert("You should go with a " + pet + ".")
 // Ask for a pet name for the pet starting with the same letter as the pet starts with
-
+var pname = prompt("...and what about their name?");
 // Display petname the pet says hi. 
+alert(pname + " the " + pet + " says hi!");
+
